611ab0995d3c8e351b4cd5fed05261a0cc62a2c2
ankit59111 <ankitsingh5991111@gmail.com>
2019-05-05 10:28:27 +0530

    configured for mobile

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
   return (
       <React.Fragment>
         <div className="App flex ">
-          <Header/>
+          <Header openNav = {openNav} closeNav = {closeNav}/>
         </div>
-        <div className={"flex flex-row"}>
-          <div className="w-75">
+          <div id="overlay"></div>
+        <div className={"flex flex-row"} id={"id_mainContentDiv"}>
+          <div className="w-75" id={"id_section"}>
             <FilterCard/>
             <Popular_sector/>
           </div>
-          <div className={"w-25"}>
+          <div className={"w-25"} id={"id_aside"}>
             <SideBarCard placeholder="Search company" heading={"Find Strategic gaps"}/>
             <SideBarCard heading={"Explore Market maps"} placeholder="Search selector"/>
             <SideBarCard placeholder="Search sector" heading={"Identify Potential acquires"}/>
@@ -25,6 +26,15 @@ function App() {
         </div>
       </React.Fragment>
   );
+}
+function openNav() {
+    document.getElementById("id_dropdown_menu").style.left="0%";
+    document.getElementById("overlay").style.display="block";
+}
+
+function closeNav() {
+    document.getElementById("id_dropdown_menu").style.left="100%";
+    document.getElementById("overlay").style.display="none";
 }
 
 export default App;
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./Header.css";
-import user from "../images/user.png"
+import user from "../images/user.png";
+import menuIcon from "../images/menu.svg"
 import SearchBar from "../searchBar/SearchBar";
 export default class Header extends React.Component{
     constructor(props){
@@ -8,10 +9,25 @@ export default class Header extends React.Component{
         this.state = {
             showDropDown: false
         }
+
     }
 
+    componentDidMount() {
+
+    }
 
     handleDropdown(e){
+        this.setState({
+            showDropDown : !this.state.showDropDown
+        })
+    }
+    handleSideNav(e){
+
+        this.props.openNav();
+        this.handleDropdown();
+    }
+    closeSidePanel(e){
+        this.props.closeNav();
         this.setState({
             showDropDown : !this.state.showDropDown
         })
@@ -21,6 +37,7 @@ export default class Header extends React.Component{
         const state = this.state;
         return(
             <nav>
+
                 <img  className="" src="https://www.kognetics.com/wp-content/uploads/2018/11/imageedit_39_8732036311.png"
                      alt="header_logo"/>
                 <SearchBar imgClassName={"show"} submitClassName={"hide"}/>
@@ -28,8 +45,15 @@ export default class Header extends React.Component{
                     <img src={user} alt={"user"}/>
                     <span>Mrinalani Saha</span>
                 </div>
+                <span id="sidenav" className={"hide"} onClick={this.handleSideNav.bind(this)}>
+                    <img src={menuIcon} alt={"menu icon"}/>
 
-                    <div className={`dropdown-menu ${state.showDropDown ? "" : "hide"}`}>
+                </span>
+
+                    <div id="id_dropdown_menu" className={`dropdown-menu ${state.showDropDown ? "" : "hide"}`}>
+                        <div className={"sideNavHeader"}>
+                            <span onClick={this.closeSidePanel.bind(this)}>close</span>
+                        </div>
                         <ul className="dropdown-menu-list">
                             <li className="list-item">
                                 Change Profile
